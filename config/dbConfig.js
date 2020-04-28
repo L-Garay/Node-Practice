def1add91e12ebcf5c51cf7561b51f4c600cbb62
Logan Garay <garay.logan@gmail.com>
2020-04-28 17:14:45 -0600

    connect db & handle unhandled erros/color log msgs

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -7,7 +7,9 @@ const connectDB = async () => {
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
-  console.log(`MongoDB connected: ${connection.connection.host}`);
+  console.log(
+    `MongoDB connected: ${connection.connection.host}`.cyan.underline.bold
+  );
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const connectDB = require('./config/dbConfig');
+//tool that allows to color/style your console.log messages -- optional
+const colors = require('colors');
 
 //Load env variables
 dotenv.config({ path: './config/config.env' });
@@ -30,6 +32,15 @@ if (process.env.NODE_ENV === 'development') {
 
 //Starting up server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error}`.red);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
